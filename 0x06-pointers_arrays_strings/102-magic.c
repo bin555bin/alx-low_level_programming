0c95d6d22c57e2f8d443a16f1668bee918b704c6
bin555bin <biniyamy500@gmail.com>
2024-01-05 04:45:52 -0800

    hg

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 int main(void)
 {
   int n;
   int a[5];
   int *p;
-
-  a[2] = 1024;
+a[2] = 1024;
   p = &n;
   /*
    * write your line of code here...
@@ -21,4 +19,3 @@ int main(void)
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
-
