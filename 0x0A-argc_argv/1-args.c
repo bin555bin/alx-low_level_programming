364028d8f7df1b818489d441f08139aa0bdda7f1
bin555bin <biniyamy500@gmail.com>
2024-01-17 04:34:45 -0800

    two of

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 
 
 /**
-* main - prints its name, followed by a new line.
+* main - prints the number of arguments passed into it.
 * @argc: argument count
 * @argv: arguments
 *
@@ -10,10 +10,10 @@
 */
 int main(int argc, char **argv)
 {
-(void)argc;
+(void)argv;
 
 
-printf("%s\n", argv[0]);
+printf("%d\n", argc - 1);
 return (0);
 }
 
